eb09a3d6aefaaedcc11ba6703da4701748e49077
Gatomek <tomasz.galinski@gmail.com>
2026-07-04 00:55:56 +0200

    Add git rev hash info to application

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,1 +1,2 @@
 declare const APP_VERSION: string;
+declare const COMMIT_HASH: string;
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
+import {execSync} from 'node:child_process';
+
+const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
     define: {
-        APP_VERSION: JSON.stringify(process.env.npm_package_version)
+        APP_VERSION: JSON.stringify(process.env.npm_package_version),
+        COMMIT_HASH: JSON.stringify(commitHash)
     },
     envDir: './env',
     base: '/project-flashcards-frontend/'
